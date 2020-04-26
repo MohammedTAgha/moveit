683b8ebd42c105a8e9de2d07959cc78905f39fdd
MOHAMMETDAGHA <MOHAMMETDAGHA@max.noreply.github.com>
2020-04-26 18:49:50 +0300

    edit api gitting method

---
+++ src/API/index.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const myKey = "cd747fb3aa0887ceb7731136b85ec09f"
+export const searchData = async ( keyward )=>{
+    let url =
+    "https://api.themoviedb.org/3/search/movie?api_key="+myKey+"&query=batman";
+
+    try {
+        const data = await axios.get(url);
+        return data;
+    } catch (error) {
+        console.log('eroor');
+    }
+
+}
